9624dfe22dab41c05a1eb40037a621fa068ac490
Cameron Levine <crlevine@Camerons-MBP.fios-router.home>
2021-01-05 15:01:13 -0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=075951a4b1ebc41e7beee31b5e9e9953&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    }
+
+    if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
